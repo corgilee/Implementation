a4a0c0f841fb0f5c49889456ff7e9597d32bf7b2
corgilee <38671352+corgilee@users.noreply.github.com>
2019-07-04 14:32:35 -0400

    NaiveBayes Implementation
    machine learning in action p67/94

--- kMeans.py
+++ kMeans.py
@@ -3,6 +3,7 @@
 ###############################
 
 import numpy as np
+import sys
 import math
 
 
@@ -35,11 +36,42 @@ def randCent(dataSet, k):
     for j in range(n):
         minJ = min(dataSet[:, j])
         rangeJ = float(max(dataSet[:, j]) - minJ)
-        centroids[:, j] = minJ + rangeJ * np.random.rand(k, 1)  # k rows and 1 col
+        p1 = minJ
+        p2 = rangeJ * np.random.rand(k, 1)
+        centroids[:, j] = p1+p2 # k rows and 1 col
     return centroids
+
+def kMeans(dataSet, k ,distMeas=distEclud,createCent = randCent):
+    m = dataSet.shape[0]
+    clusterAssment = np.mat(np.zeros((m, 2)))
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):
+            # loop for each ob in the dataSet, row i
+            minDist = sys.maxsize
+            minIndex = -1
+            for j in range(k):
+                # loop for each centroid, centroids, every row (j), has a vector for the certain centroids
+                distJI=distMeas(centroids[j,:],dataSet[i,:])
+                if distJI < minDist:
+                    minDist = distJI
+                    minIndex = j
+            #check if ob i's group keep same
+            if clusterAssment[i, 0] !=minIndex:
+                clusterChanged = True
+            clusterAssment[i, :] = minIndex, minDist**2  # update index, mindist
+        for cent in range(k):
+            # np.nonzero, check the notes in onenote
+            ptsInClust = dataSet[np.nonzero(clusterAssment[:, 0].A == cent)[0]]  # get the row/index number of of the none-zero row
+        centroids[cent, :] = np.mean(ptsInClust, axis=0)  # average the vector along row direction
+    return centroids, clusterAssment
 
 
 if __name__ == "__main__":
-    dataMat = loadDataSet('testSet.txt')
-    print(randCent(dataMat, 2))
-    print(distEclud(dataMat[0], dataMat[1]))
+    datMat = loadDataSet('testSet.txt')
+    myCentroids, clustAssing = kMeans(datMat, 4)
+    print(myCentroids)
+    print(clustAssing)
+
