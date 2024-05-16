270391498b5360ed8425fbdebe170fcf57492996
Wenjie Zhao <wenjie@squareup.com>
2024-05-15 20:36:26 -0700

    edit files

--- notebook/ml_integration.py
+++ notebook/ml_integration.py
@@ -1,1 +1,38 @@
-print('hello world')
+'''
+先把步骤讲出来，问问对方可不可以
+
+1. Data exploration
+    1. flag (positive rate, 一般可能是pos_rate 低的数据，后面要重点讨论 precision，recall，f1 score 和threshold，还有需不需要downsample）
+    2. check_missing_rate (decide if some variables need to be dropped)
+    3. data_type (numerical+categorical）
+2. Feature engineering
+    1. missing imputation (numerical data + categorical data)
+    2. categorical data transformation
+        1. check category high cardinal, if so, may use target encoding
+    3. scale (optional)
+3. Training data build up
+    1. split
+        1. 确认一下是否需要downsample negative 
+4. Model training
+    1. (先讲一讲， logistic regression, random forest , gbm tree 区别）
+    2. model fit
+    3. feature importance
+    4. cross validation (optional)
+'''
+
+import pandas as pd
+import numpy as np
+
+### Data Type
+
+df=pd.read_csv('data.csv')
+df.info()
+data.dtypes.value_counts()
+
+
+# category/numerical
+cat_features=df.select_dtypes(object).columns.tolist()
+
+feature1=df.select_dtypes('float64').columns.tolist()
+feature2=df.select_dtypes(int).columns.tolist()
+num_features=feature1+feature2
